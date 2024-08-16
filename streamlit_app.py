c6a3d5730724ea313d0f532287951da15c6d4331
JN <JN@outlook.com>
2024-08-16 21:26:21 +0000

    fix: Set DEBUG_MODE to true

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from tavily import TavilyClient
 from workflow_steps import WORKFLOW_STEPS, SUMMARY_BEGINNING_OF_PROMPT, SUMMARY_END_OF_PROMPT
 
 # Set DEBUG_MODE
-DEBUG_MODE = False
+DEBUG_MODE = True
 
 st.set_page_config(page_title="JN test - Company Analysis Workflow")
 st.title("JN test - Company Analysis Workflow")
